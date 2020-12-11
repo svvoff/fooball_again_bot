18783c4bb2905f0c56cee98c86dfafae3c602786
Andrey Sorokin <as.sorokin@corp.vk.com>
2020-12-11 21:55:16 +0300

    dealy between batch of handled messages, update triggers without restart, try to handle emojies

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from pathlib import Path
 import os
 import logging
 import json
+import datetime as dt
+import unicodedata
 
 # Logging
 logging.basicConfig(level=logging.DEBUG,
@@ -17,35 +19,80 @@ load_dotenv(dotenv_path=env_path)
 
 TOKEN = os.getenv('TOKEN')
 
-
-def get_triggers() -> list:
-    with open('footbick.json') as json_file:
-        data = json.load(json_file)
-        return data['triggers']
-
-TRIGGERS = get_triggers()
-
-# Run
-updater = Updater(token=TOKEN)
-
-def update_triggers(update: Update, context: CallbackContext) -> None:
-    TRIGGERS = get_triggers()
-    message = update.message
-    new_triggers = "\n".join(TRIGGERS)
-    message.reply_text("Triggers were updated")
-    context.bot.send_message(chat_id=update.effective_chat.id, text=new_triggers)
+class App:
+    def __init__(self, token):
+        self.updater = Updater(token=token)
+        self.update_triggers()
+        self.messages_counter = 3
+        self.last_handled_message_date = None
+        self.inter_messages_dealy_seconds = 10
     
-def messages_handler(update: Update, context: CallbackContext) -> None:
-    message = update.message
-    text = message.text.lower()
-    for trigger in TRIGGERS:
-        if trigger in text:
-            message.reply_text("Опять футбик")
-            break
+    def run(self):
+        updater = self.updater
+        updater.dispatcher.add_handler(CommandHandler('update_triggers', self.update_triggers))
+        updater.dispatcher.add_handler(MessageHandler(Filters.all, self.messages_handler))
 
+        updater.start_polling()
+        updater.idle()
+
+
+    def update_triggers(self, update: Update, context: CallbackContext) -> None:
+        self.update_triggers()
+        message = update.message
+        new_triggers = "\n".join(self.triggers)
+        message.reply_text("Triggers were updated")
+        context.bot.send_message(chat_id=update.effective_chat.id, text=new_triggers)
     
-updater.dispatcher.add_handler(CommandHandler('update_triggers', update_triggers))
-updater.dispatcher.add_handler(MessageHandler(Filters.all, messages_handler))
+    def messages_handler(self, update: Update, context: CallbackContext) -> None:
+        message = update.message
+        
+        text = None
+        emoji = False
+        if message.text:
+            text = message.text.lower()
+        elif message.dice:
+            emoji = True
+            text = message.dice.emoji
 
-updater.start_polling()
-updater.idle()
+        if text == None:
+            return
+
+        if emoji:
+            for e in self.emojis:
+                if str(e) == text:
+                    self.reply_if_needed(update = update, context = context)
+                    break
+
+        else:
+            for trigger in self.triggers:
+                if trigger in text:
+                    self.reply_if_needed(update = update, context = context)
+                    break
+                
+    
+    def reply_if_needed(self, update: Update, context: CallbackContext) -> None:
+        message = update.message
+        if self.last_handled_message_date != None:
+            diff = (message.date - self.last_handled_message_date).total_seconds()
+            if diff < self.inter_messages_dealy_seconds:
+                return
+            else:
+                self.last_handled_message_date = None
+
+        self.messages_counter -= 1
+        if self.messages_counter <= 0:
+            self.messages_counter = 10
+            self.last_handled_message_date = message.dat
+        msg = "Опять футбик"
+        message.reply_text(msg)
+
+    def update_triggers(self):
+        with open('footbick.json') as json_file:
+            data = json.load(json_file)
+            self.triggers = data['triggers']
+            self.emojis = data['emojis']
+
+app = App(TOKEN)
+app.run()
+    
+
